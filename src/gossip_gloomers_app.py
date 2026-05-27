908d63442d0ba0168757eb02166410b2efbffe92
natalka1122 <natalka1122@users.noreply.github.com>
2026-05-27 20:24:40 +0200

    fix formatting: black line-length compliance

--- src/gossip_gloomers_app.py
+++ src/gossip_gloomers_app.py
@@ -9,7 +9,9 @@ from message import BodyEcho, BodyEchoOk, BodyInit, BodyInitOk, Message
 logger = get_logger(__name__)
 
 
-async def connect_stdin_stdout() -> tuple[asyncio.StreamReader, asyncio.StreamWriter]:  # noqa: WPS210
+async def connect_stdin_stdout() -> (
+    tuple[asyncio.StreamReader, asyncio.StreamWriter]
+):  # noqa: WPS210
     loop = asyncio.get_running_loop()
     reader = asyncio.StreamReader()
     protocol = asyncio.StreamReaderProtocol(reader)
